76b393fa809ff79f8ff69c3a6642f867e8fe3380
Legenda759 <oripov1994@gmail.com>
2022-01-17 17:12:15 +0500

    MyDemo SwiftUI file viewed as main and Text changed to "Abdurahmon Hursanboyev"

---
+++ MyDemo/MyDemo.swift
@@ -0,0 +1,20 @@
+//
+//  MyDemo.swift
+//  MyDemo
+//
+//  Created by Legenda_759 on 17/01/22.
+//
+
+import SwiftUI
+
+struct MyDemo: View {
+    var body: some View {
+        Text("Abdurahmon Hursanboyev")
+    }
+}
+
+struct MyDemo_Previews: PreviewProvider {
+    static var previews: some View {
+        MyDemo()
+    }
+}
