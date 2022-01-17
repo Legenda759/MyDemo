429db473200e4bf64f6fd1bdabef823145576090
Legenda759 <oripov1994@gmail.com>
2022-01-17 17:05:37 +0500

    Initial Commit

---
+++ MyDemo/MyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoApp.swift
+//  MyDemo
+//
+//  Created by Legenda_759 on 17/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
